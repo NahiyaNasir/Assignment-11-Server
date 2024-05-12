096ddaee16d58ec6c3754819b8ea8ea808237864
NahiyaNasir <nahiaesha.01@gmail.com>
2024-05-12 15:55:32 +0600

    get api by id

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(
     cors({
       origin:  
                        [
-       "http://localhost:5173"
+       "http://localhost:5173",
+       "https://assigment-11-server-two.vercel.app"
       ],
       credentials: true,
     })
@@ -41,12 +42,25 @@ async function run() {
     // Send a ping to confirm a successful connection
 
     const volunteerCollection=client.db('volunteerDB').collection('management')
+    const requestedCollection=client.db('volunteerDB').collection('requested')
      app.get('/volunteer',async(req,res)=>{
        const curser=volunteerCollection.find()
        const result= await curser.toArray()
        res.send(result)
      })
-    //  create data
+    //  for search
+    //  app.get('/volunteer',async(req,res)=>{
+    //   const search=req.body.search
+    //   console.log(search)
+    //   let quarry={
+    //     title:{$regex :search ,$options:'i'}
+    //   }
+    //   const result=volunteerCollection.findOne(quarry)
+    //   res.send(result)
+    //   console.log(result)
+    //  })
+
+    //  create data for all
    app.post('/volunteer',async(req,res)=>{
     const newItem = req.body;
     // console.log(newItem);
@@ -57,11 +71,21 @@ async function run() {
     //  get id for details
    app.get("/volunteer/:id", async (req, res) => {
     const id = req.params.id;
-    console.log(id);
+    // console.log(id);
     const quarry = { _id: new ObjectId(id) };
     const result = await volunteerCollection.findOne(quarry);
     res.send(result);
   })
+  //   create new collection
+    app.post('/requested',async(req,res)=>{
+      const reqItem=req.body
+      console.log(reqItem)
+      const result=await requestedCollection.insertOne(reqItem)
+      res.send(result)
+      // const decreesResult=await volunteerCollection.updateOne({$inc:{ number:-1}})
+      // res.send(decreesResult)
+    })
+
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
