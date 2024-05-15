b82b5142c92b7540210b61ca5f1948e8a82e435f
NahiyaNasir <nahiaesha.01@gmail.com>
2024-05-15 15:08:40 +0600

    update data

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -6,15 +8,15 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 app.use(express.json());
 // const corsConfig = {
-//   origin: '',
+//   origin: '*',
 //   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE']
+//   methods: ['GET', 'POST', 'PUT', "PATCH",'DELETE']
 // }
 // app.use(cors(corsConfig))
 // app.options("", cors(corsConfig))
 app.use(cors());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gze7wpc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -43,17 +45,6 @@ async function run() {
       const result = await curser.toArray();
       res.send(result);
     });
-    // //  for search
-    //  app.get('/volunteers',async(req,res)=>{
-    //   const search=req.body.search
-    //   console.log(search)
-    //   let quarry={
-    //     title:{$regex :search ,$options:'i'}
-    //   }
-    //   const result=volunteerCollection.findOne(quarry)
-    //   res.send(result)
-    //   console.log(result)
-    //  })
 
     //  create data for all
     app.post("/volunteer", async (req, res) => {
@@ -79,7 +70,23 @@ async function run() {
       const result = await volunteerCollection.find(query).toArray();
       res.send(result);
     });
-   
+    
+    //  get data by sorting
+    app.get('/volunteer-upComing-deadline',async(req,res)=>{
+    
+      const result= await  volunteerCollection.find().sort({deadline:1}).toArray();
+      res.send(result)
+    })
+    //  for search
+    app.get("/volunteer-searchBy-title", async (req, res) => {
+      const search = req.query.search;
+      let query = {
+        title: { $regex: search, $options: "i" },
+      };
+      const result = await volunteerCollection.find(query).toArray();
+      res.send(result);
+    });
+
     //  get update data id
     app.get("/single-volunteer-update/:id", async (req, res) => {
       const id = req.params.id;
@@ -133,38 +140,59 @@ async function run() {
       // console.log(result);
     });
 
- //   create new collection
- app.post("/requested", async (req, res) => {
-  const reqItem = req.body;
-  console.log(reqItem);
-  const result = await requestedCollection.insertOne(reqItem);
-  res.send(result);
-  // const decreesResult=await volunteerCollection.updateOne({$inc:{ number:-1}})
-  // res.send(decreesResult)
-});
-//  get all requested data by  email
-app.get("/requested-volunteer-email", async (req, res) => {
-  const email = req.query.email;
-  console.log(email);
-  const query = { email: email };
-  const result = await requestedCollection.find(query).toArray();
-  res.send(result);
-});
-  //   delete form requested collection
-  app.get("/requested-volunteer-delete/:id",async(req,res)=>{
-    const id=req.params.id
-    const quarry={_id: new ObjectId(id)}
-    const result=await requestedCollection.findOne(quarry)
-    res.send(result)
-  })
-  app.delete("/requested-volunteer-delete/:id",async(req,res)=>{
-    const id = req.params.id;
+    //   create new collection
+    app.post("/requested", async (req, res) => {
+      const reqItem = req.body;
+      console.log(reqItem);
+      const result = await requestedCollection.insertOne(reqItem);
+      res.send(result);
+     
+    });
+    //  get all requested data by  email
+    app.get("/requested-volunteer-email", async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      const query = { email: email };
+      const result = await requestedCollection.find(query).toArray();
+      res.send(result);
+    });
+    //   delete form requested collection
+    app.get("/requested-volunteer-delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const quarry = { _id: new ObjectId(id) };
+      const result = await requestedCollection.findOne(quarry);
+      res.send(result);
+    });
+    app.delete("/requested-volunteer-delete/:id", async (req, res) => {
+      const id = req.params.id;
       // console.log(id);
       const quarry = { _id: new ObjectId(id) };
       const result = await requestedCollection.deleteOne(quarry);
       res.send(result);
-  })
-
+    });
+    //  get id for patch
+    app.get("/requested-volunteer-inc/:id",async(req,res)=>{
+      const id=req.params.id
+      const quarry={_id:new ObjectId(id)}
+      const result=await requestedCollection.findOne(quarry)
+      res.send(result)
+    })
+    //  increment the quantity
+    app.put("/requested-volunteer-inc/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+     
+      
+      const updateDoc = {
+        $inc: { number: -1 } 
+        
+      };
+      
+      const result = await volunteerCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    
+    });
     // await client.db("admin").command({ ping: 1 });
     // console.log(
     //   "Pinged your deployment. You successfully connected to MongoDB!"
